807b398fd6ae40fabdf1c771d09485751eee129a
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-03-14 17:45:20 -0300

    Desenvolvido um modelo de dados a serem recebidos desenvolvido um menu para poder digitar diferente nomes de séries e refatorado do main do aplicativo

--- src/main/java/br/com/project/catalogodeseries/CatalogoDeSeriesApplication.java
+++ src/main/java/br/com/project/catalogodeseries/CatalogoDeSeriesApplication.java
@@ -1,12 +1,11 @@
 package br.com.project.catalogodeseries;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.project.catalogodeseries.model.DadosSerie;
-import br.com.project.catalogodeseries.service.ConsumoApi;
-import br.com.project.catalogodeseries.service.ConverterDados;
+import br.com.project.catalogodeseries.main.Principal;
 
 @SpringBootApplication
 public class CatalogoDeSeriesApplication implements CommandLineRunner {
@@ -17,14 +16,8 @@ public class CatalogoDeSeriesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverterDados conversor = new ConverterDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		var principal = new Principal();
+		principal.exibeMenu();
 	}
 
 
